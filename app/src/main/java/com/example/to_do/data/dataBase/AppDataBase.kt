37ca21ce18f59b1e1cf29889d5d5dce4518bbade
Anderson2A <112587285+Anderson-A2ilva@users.noreply.github.com>
2024-10-21 19:38:57 -0300

    Feat: Dialog informações atividade

--- app/src/main/java/com/example/to_do/data/dataBase/AppDataBase.kt
+++ app/src/main/java/com/example/to_do/data/dataBase/AppDataBase.kt
@@ -4,21 +4,18 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import androidx.room.TypeConverters
-import com.example.to_do.converters.Converters
-import com.example.to_do.data.dataBase.dao.AtividadeDao
 import com.example.to_do.data.dataBase.dao.FormularioDao
+import com.example.to_do.data.dataBase.migrations.MIGRATION_1_2
 import com.example.to_do.data.model.Atividade
 
 @Database(
     entities = [Atividade::class],
-    version = 1,
+    version = 2,
     exportSchema = true
 )
-@TypeConverters(Converters::class)
+
 abstract class AppDataBase : RoomDatabase() {
     abstract fun formularioDao(): FormularioDao
-    abstract fun atividadeListaDao(): AtividadeDao
 
     companion object {
         @Volatile
@@ -28,7 +25,7 @@ abstract class AppDataBase : RoomDatabase() {
                 context,
                 AppDataBase::class.java,
                 "atividade.db"
-            ).build()
+            ).addMigrations(MIGRATION_1_2).build()
                 .also {
                     db = it
                 }
